ad6565b9cd3afac4e4fc510dedf696ba6a2ecb61
norkz <59128189+norkz@users.noreply.github.com>
2021-02-28 23:38:33 -0300

    el perfeccion

---
+++ index.js
@@ -0,0 +1,24 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+console.log('Press Any keyboard key to add the value and Arrow_Left to decrease')
+setInterval(() => {
+  setInterval(() => {
+    setInterval(() => {
+      setInterval(() => {
+        setInterval(() => {
+          setInterval(() => {
+            setInterval(() => {
+              console.log(`${rl.cursor.toString().replace(/^/, 'Heartbeat number: ')}\n${rl.cursor.toString().replace(/^/, 'Heartbeat number: ')}\n${rl.cursor.toString().replace(/^/, 'Heartbeat number: ')}`)
+            }, 0);// Interception
+          }, 0);// Interception
+        }, 0);// Interception
+      }, 0);// Interception
+    }, 0);// Interception
+  }, 0);// Interception
+}, 0);// Interception
+
